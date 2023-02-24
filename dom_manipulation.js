7bde8d0c41144501e2d1dc1d678bd65dffc2792d
abbasalit987 <abbasalit987@gmail.com>
2023-02-24 18:44:27 +0530

    dom manipulation commit

---
+++ dom_manipulation.js
@@ -0,0 +1,6 @@
+//console.dir(document);
+//console.log(document.title);
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 30px #000';
+var add_item = document.getElementById('main');
+add_item.children[0].innerHTML = '<h1 style="font-color:green">ADD ITEMS</h1>'
